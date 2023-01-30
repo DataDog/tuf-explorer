ec65bc661c471150986e77a5156d1b1b7497b5ea
Cédric Van Rompay <cedric.vanrompay@datadoghq.com>
2023-01-30 12:29:57 +0100

    add legal prefix in svelte files

--- src/routes/diff/+page.svelte
+++ src/routes/diff/+page.svelte
@@ -1,4 +1,10 @@
 <script lang="ts">
+/**
+Unless explicitly stated otherwise all files in this repository are licensed under the Apache-2 License.
+
+This product includes software developed at Datadog (https://www.datadoghq.com/)  Copyright 2023-present Datadog, Inc.
+**/
+
     import BreadCrumbs from "$lib/BreadCrumbs.svelte";
     import { getFromRevision, getToRevision } from "$lib/git";
     import type { PageData } from "./$types";
